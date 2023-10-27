eb952763af0da59760e45cbdda7fef870294db32
Kevis <kevis@Zipcoders-MacBook-Pro-82.local>
2023-10-27 18:01:14 -0400

    still working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,14 +7,16 @@ public class Main {
 //learned scanner comm req. activation command in order to use
     import java.util.Scanner;
 //
-    public static void main(String[] args){
+public static void main(String[] args){
       //need a loop to run the game
-        for (int i = 0; i < 10; i++) { System.out.printIn("");
+    for (int i = 0; i < 10; i++) { System.out.printIn("");
             // need random generator
-        Random rand = new randomNum();
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(10) + 1;
+        System.out.printIn(" enter guess (1-10)");
 // need IF statement
        // "too large", "too small", "correct guess",
-        if (userGuess > randomNum){
+    if (userGuess > Random){
             System.out.printIn("too large");}
 
         else if (userGuess < Random) {
