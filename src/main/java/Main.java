5c9084b58615cca1d67962538f4c34a5e68c4635
Kevis <kevis@Zipcoders-MacBook-Pro-82.local>
2023-10-27 07:47:24 -0400

    putting it together

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,29 @@
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
+//learned random comm req. activation command in order to use
+    import java.util.Random;
+//learned scanner comm req. activation command in order to use
+    import java.util.Scanner;
+//
     public static void main(String[] args){
-        
+      //need a loop to run the game
+        for (int i = 0; i < 10; i++) { System.out.printIn("");
+            // need random generator
+        Random rand = new randomNum();
+// need IF statement
+       // "too large", "too small", "correct guess",
+        if (userGuess > randomNum){
+            System.out.printIn("too large");}
+
+        else if (userGuess < Random) {
+            System.out.printIn ("too small");}
+
+        else(userGuess == Random){
+            System.out.printIn ("correct guess");
+        }
+// code a user pick comm
+        Scanner scanner = new Scanner(System.in);
+        int userGuess = scanner.nextInt();
     }
 }
